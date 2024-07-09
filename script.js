adfdb87a9f03b1d5fbde80c37c5c03ff5085ad16
Beka Chogovadze <chogovadzebeka@gmail.com>
2024-07-09 21:18:27 +0400

    second commit  of drag and drop page

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const editor = grapesjs.init({
     container: '#gjs',
     fromElement: true,
@@ -10,7 +9,19 @@ const editor = grapesjs.init({
         autoload: true, // Enable autoload
         stepsBeforeSave: 1, // Save after each change
     },
-    panels: { defaults: [] },
+    deviceManager: {
+        devices: [
+            {
+                name: 'Desktop',
+                width: '', // default size
+            }, 
+            {
+                name: 'Mobile',
+                width: '320px', // this value will be used on canvas width
+                widthMedia: '480px', // this value will be used in CSS @media
+            }
+        ]
+    },
     blockManager: {
         appendTo: '#blocks',
         blocks: [
@@ -56,30 +67,182 @@ const editor = grapesjs.init({
             }
         ]
     },
-
     layerManager: {
         appendTo: '.layers-container'
-      },
-      // We define a default panel as a sidebar to contain layers
-      panels: {
-        defaults: [{
-          id: 'layers',
-          el: '.panel__right',
-          // Make the panel resizable
-          resizable: {
-            maxDim: 350,
-            minDim: 200,
-            tc: 0, // Top handler
-            cl: 1, // Left handler
-            cr: 0, // Right handler
-            bc: 0, // Bottom handler
-            // Being a flex child we need to change `flex-basis` property
-            // instead of the `width` (default)
-            keyWidth: 'flex-basis',
-          },
-        }]
-      }
-    });
+    },
+    selectorManager: {
+        appendTo: '.styles-container'
+    },
+    styleManager: {
+        appendTo: '.styles-container',
+        sectors: [
+            {
+                name: 'Dimension',
+                open: false,
+                buildProps: ['width', 'min-height', 'padding'],
+                properties: [
+                    {
+                        type: 'integer',
+                        name: 'The width',
+                        property: 'width',
+                        units: ['px', '%'],
+                        defaults: 'auto',
+                        min: 0,
+                    }
+                ]
+            },
+            {
+                name: 'Extra',
+                open: false,
+                buildProps: ['background-color', 'box-shadow', 'custom-prop'],
+                properties: [
+                    {
+                        id: 'custom-prop',
+                        name: 'Custom Label',
+                        property: 'font-size',
+                        type: 'select',
+                        defaults: '32px',
+                        options: [
+                            { value: '12px', name: 'Tiny' },
+                            { value: '18px', name: 'Medium' },
+                            { value: '32px', name: 'Big' },
+                        ],
+                    }
+                ]
+            }
+        ]
+    },
+    panels: {
+        defaults: [
+            {
+                id: 'panel-top',
+                el: '.panel__top',
+            },
+            {
+                id: 'basic-actions',
+                el: '.panel__basic-actions',
+                buttons: [
+                    {
+                        id: 'visibility',
+                        active: true, // active by default
+                        className: 'btn-toggle-borders',
+                        label: '<u>B</u>',
+                        command: 'sw-visibility', // Built-in command
+                    },
+                    {
+                        id: 'export',
+                        className: 'btn-open-export',
+                        label: 'Exp',
+                        command: 'export-template',
+                        context: 'export-template', // For grouping context of buttons from the same panel
+                    },
+                    {
+                        id: 'show-json',
+                        className: 'btn-show-json',
+                        label: 'JSON',
+                        context: 'show-json',
+                        command(editor) {
+                            editor.Modal.setTitle('Components JSON')
+                                .setContent(`<textarea style="width:100%; height: 250px;">
+                                    ${JSON.stringify(editor.getComponents())}
+                                </textarea>`)
+                                .open();
+                        },
+                    }
+                ],
+            },
+            {
+                id: 'layers',
+                el: '.panel__right',
+                resizable: {
+                    maxDim: 350,
+                    minDim: 200,
+                    tc: 0, // Top handler
+                    cl: 1, // Left handler
+                    cr: 0, // Right handler
+                    bc: 0, // Bottom handler
+                    keyWidth: 'flex-basis',
+                },
+            },
+            {
+                id: 'panel-switcher',
+                el: '.panel__switcher',
+                buttons: [
+                    {
+                        id: 'show-layers',
+                        active: true,
+                        label: 'Layers',
+                        command: 'show-layers',
+                        togglable: false,
+                    },
+                    {
+                        id: 'show-style',
+                        active: true,
+                        label: 'Styles',
+                        command: 'show-styles',
+                        togglable: false,
+                    },
+                ],
+            },
+            {
+                id: 'panel-devices',
+                el: '.panel__devices',
+                buttons: [
+                    {
+                        id: 'device-desktop',
+                        label: 'D',
+                        command: 'set-device-desktop',
+                        active: true,
+                        togglable: false,
+                    },
+                    {
+                        id: 'device-mobile',
+                        label: 'M',
+                        command: 'set-device-mobile',
+                        togglable: false,
+                    },
+                ],
+            }
+        ]
+    }
+});
+
+// Define commands
+editor.Commands.add('show-layers', {
+    getRowEl(editor) { return editor.getContainer().closest('.editor-row'); },
+    getLayersEl(row) { return row.querySelector('.layers-container') },
+
+    run(editor, sender) {
+        const lmEl = this.getLayersEl(this.getRowEl(editor));
+        lmEl.style.display = '';
+    },
+    stop(editor, sender) {
+        const lmEl = this.getLayersEl(this.getRowEl(editor));
+        lmEl.style.display = 'none';
+    },
+});
+
+editor.Commands.add('show-styles', {
+    getRowEl(editor) { return editor.getContainer().closest('.editor-row'); },
+    getStyleEl(row) { return row.querySelector('.styles-container') },
+
+    run(editor, sender) {
+        const smEl = this.getStyleEl(this.getRowEl(editor));
+        smEl.style.display = '';
+    },
+    stop(editor, sender) {
+        const smEl = this.getStyleEl(this.getRowEl(editor));
+        smEl.style.display = 'none';
+    },
+});
+
+editor.Commands.add('set-device-desktop', {
+    run: editor => editor.setDevice('Desktop')
+});
+
+editor.Commands.add('set-device-mobile', {
+    run: editor => editor.setDevice('Mobile')
+});
 
 // Wait for the editor to be fully loaded
 editor.on('load', () => {
@@ -96,50 +259,12 @@ editor.on('load', () => {
     });
 });
 
-// Add panels and buttons
-editor.Panels.addPanel({
-    id: 'panel-top',
-    el: '.panel__top',
-});
-
-editor.Panels.addPanel({
-    id: 'basic-actions',
-    el: '.panel__basic-actions',
-    buttons: [
-        {
-            id: 'visibility',
-            active: true, // active by default
-            className: 'btn-toggle-borders',
-            label: '<u>B</u>',
-            command: 'sw-visibility', // Built-in command
-        },
-        {
-            id: 'export',
-            className: 'btn-open-export',
-            label: 'Exp',
-            command: 'export-template',
-            context: 'export-template', // For grouping context of buttons from the same panel
-        },
-        {
-            id: 'show-json',
-            className: 'btn-show-json',
-            label: 'JSON',
-            context: 'show-json',
-            command(editor) {
-                editor.Modal.setTitle('Components JSON')
-                    .setContent(`<textarea style="width:100%; height: 250px;">
-                        ${JSON.stringify(editor.getComponents())}
-                    </textarea>`)
-                    .open();
-            },
-        }
-    ],
-});
+// Listen to export-template events
 editor.on('run:export-template:before', opts => {
     console.log('Before the command run');
     if (0 /* some condition */) {
-      opts.abort = 1;
+        opts.abort = 1;
     }
-  });
-  editor.on('run:export-template', () => console.log('After the command run'));
-  editor.on('abort:export-template', () => console.log('Command aborted'));
+});
+editor.on('run:export-template', () => console.log('After the command run'));
+editor.on('abort:export-template', () => console.log('Command aborted'));
